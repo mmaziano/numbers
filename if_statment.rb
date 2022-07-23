e383dd267de6364b3781a7795166da860fb10a5a
Mason Maziano <mason.k.maziano@gmail.com>
2022-07-23 14:22:05 +0100

    Removed unnecessary comments and added one line of comment

--- if_statment.rb
+++ if_statment.rb
@@ -1,16 +1,4 @@
-#ismale = true
-#istall = false
-
-#if ismale and istall
-    #puts "You are a tall male"
-#elsif ismale and !istall
-    #puts "You are a short man"
-#elsif !ismale and istall
-    #puts "You are not male but you are tall"
-#else 
-    #puts "You are not male and not tall"
-#end
-
+# Check what is the highest number
 def max(num1, num2, num3)
   if num1 >= num2 && num1 >= num3
     puts num1
